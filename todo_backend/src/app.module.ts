87bd622c64f4a2dd33b68bbe783012c5b0cbad80
ApposoftTech <apposofttech@gmail.com>
2025-03-03 08:52:03 +0300

    Task entity, controller, module, service created with Swagger configuration

--- todo_backend/src/app.module.ts
+++ todo_backend/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskModule } from './task/task.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TaskModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT!) || 5432,
+      username: process.env.DB_USER || 'todo_user',
+      password: process.env.DB_PASS || 'todo_password',
+      database: process.env.DB_NAME || 'todo_database',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TaskModule,
+  ],
 })
 export class AppModule {}
